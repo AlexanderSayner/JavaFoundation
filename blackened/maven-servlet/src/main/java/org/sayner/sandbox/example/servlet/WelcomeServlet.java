5bff81aa22333e52ed1bea311ff341e78b2e3f57
Alexander <alexandr.deffirst@gmail.com>
2020-07-13 11:21:24 +0400

    Maven servlet project

--- blackened/maven-servlet/src/main/java/org/sayner/sandbox/example/servlet/WelcomeServlet.java
+++ blackened/maven-servlet/src/main/java/org/sayner/sandbox/example/servlet/WelcomeServlet.java
@@ -1,20 +1,39 @@
 package org.sayner.sandbox.example.servlet;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet("/")
+@WebServlet("/welcome")
 public class WelcomeServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException {
+        final HttpSession session = req.getSession();
+
+        Integer visitCounter = (Integer) session.getAttribute("visitCounter");
+        if (visitCounter == null) {
+            visitCounter = 1;
+        } else {
+            visitCounter++;
+        }
+        session.setAttribute("visitCounter", visitCounter);
+
+        final String username = req.getParameter("username");
+
         resp.setContentType("text/html");
-        PrintWriter printWriter = resp.getWriter();
-        printWriter.write("Welcome for my servlet!");
+        final PrintWriter printWriter = resp.getWriter();
+        printWriter.write("Welcome for my servlet!" + "<br>");
+        if (username == null) {
+            printWriter.write("Hello, Anonymous" + "<br>");
+        } else {
+            printWriter.write("Hello, " + username + "<br>");
+        }
+        printWriter.write("Page was visited " + visitCounter + " times.");
         printWriter.close();
     }
 }
