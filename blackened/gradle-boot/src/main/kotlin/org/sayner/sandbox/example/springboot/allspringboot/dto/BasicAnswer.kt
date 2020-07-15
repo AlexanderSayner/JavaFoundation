0a6f1ea1639e4c4af669641e610427a6fa3d0106
AlexanderSayner <alexandr.deffirst@gmail.com>
2020-07-16 01:04:33 +0400

    Spring Boot init

---
+++ blackened/gradle-boot/src/main/kotlin/org/sayner/sandbox/example/springboot/allspringboot/dto/BasicAnswer.kt
@@ -0,0 +1,6 @@
+package org.sayner.sandbox.example.springboot.allspringboot.dto
+
+data class BasicAnswer(
+        val code: String,
+        val message: String
+)
---
+++ blackened/gradle-boot/src/test/kotlin/org/sayner/sandbox/example/springboot/allspringboot/AllSpringBootApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.sayner.sandbox.example.springboot.allspringboot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AllSpringBootApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
