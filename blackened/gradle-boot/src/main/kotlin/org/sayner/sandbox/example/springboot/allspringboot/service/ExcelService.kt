bed98430b4e2ca2ac4ed3e86ee08e8fa8046d910
AlexanderSayner <alexandr.deffirst@gmail.com>
2020-07-19 17:48:30 +0400

    Redis uploading

--- blackened/gradle-boot/src/main/kotlin/org/sayner/sandbox/example/springboot/allspringboot/service/ExcelService.kt
+++ blackened/gradle-boot/src/main/kotlin/org/sayner/sandbox/example/springboot/allspringboot/service/ExcelService.kt
@@ -2,6 +2,7 @@ package org.sayner.sandbox.example.springboot.allspringboot.service
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.sayner.sandbox.example.springboot.allspringboot.model.Ruler
+import org.sayner.sandbox.example.springboot.allspringboot.redis.repository.RulerRepository
 import org.sayner.sandbox.example.springboot.allspringboot.util.xlsToModel
 import org.sayner.sandbox.example.springboot.allspringboot.util.xlsxToModel
 import org.slf4j.LoggerFactory
@@ -9,21 +10,28 @@ import org.springframework.stereotype.Service
 import org.springframework.web.multipart.MultipartFile
 
 @Service
-class ExcelService {
+class ExcelService(
+        private val rulerRepository: RulerRepository
+) {
     private val logger = LoggerFactory.getLogger(ExcelService::class.java)
 
     fun saveXlsx(file: MultipartFile) {
         val xlsxList = xlsxToModel(file.inputStream)
-        printList(xlsxList)
+        printList(xlsxList.distinct())
+        logger.info("${saveToRedisList(xlsxList.distinct()).count()} records saved")
     }
 
     fun saveXls(file: MultipartFile) {
         val xlsList = xlsToModel(file.inputStream)
-        printList(xlsList)
+        printList(xlsList.distinct())
+        logger.info("${saveToRedisList(xlsList.distinct()).count()} records saved")
     }
 
-    fun printList(list: List<Ruler>){
-        val objectMapper =ObjectMapper()
+    fun saveToRedisList(list: List<Ruler>): MutableIterable<Ruler> =
+            rulerRepository.saveAll(list)
+
+    fun printList(list: List<Ruler>) {
+        val objectMapper = ObjectMapper()
         list.forEach { ruler ->
             logger.info(objectMapper.writeValueAsString(ruler))
         }
